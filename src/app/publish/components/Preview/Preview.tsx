16b8d8d04d1e1345c5550666d9faf525e24c6579
Danilo <nilomiranda3@gmail.com>
2024-10-09 20:13:45 -0700

    adjusting layout, rendering markdown preview

---
+++ src/app/publish/components/Preview/Preview.tsx
@@ -0,0 +1,35 @@
+import Markdown from "react-markdown";
+import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter'
+import {darcula} from 'react-syntax-highlighter/dist/esm/styles/prism'
+import './styles.css'
+
+interface PreviewProps {
+  content: string;
+}
+
+export const Preview = ({ content }: PreviewProps) => {
+  return (
+    <div className="jcms-markdown-preview py-2 px-4">
+      <Markdown
+        components={{
+          code(props) {
+            const {children, className, node, ...rest} = props
+            const match = /language-(\w+)/.exec(className || '')
+            return match ? (
+              <SyntaxHighlighter
+                {...rest}
+                PreTag="div"
+                language={match[1]}
+                style={darcula}
+              >{String(children).replace(/\n$/, '')}</SyntaxHighlighter>
+            ) : (
+              <code {...rest} className={className}>
+                {children}
+              </code>
+            )
+          }
+        }}
+      >{content}</Markdown>
+    </div>
+  )
+}
--- src/app/publish/page.tsx
+++ src/app/publish/page.tsx
@@ -4,6 +4,8 @@ import { useState } from 'react'
 import { Bold, Italic, List, AlignLeft, Send } from 'lucide-react'
 import { Button } from "@/components/ui/button"
 import { Textarea } from "@/components/ui/textarea"
+import {Preview} from "@/app/publish/components/Preview/Preview";
+import {mdContentExample} from "@/app/publish/components/Preview/contentMockup";
 
 export default function PublishPage() {
   const [content, setContent] = useState('')
@@ -16,37 +18,49 @@ export default function PublishPage() {
   }
 
   return (
-    <div className="max-w-2xl mx-auto p-4 bg-white shadow-lg rounded-lg">
-      <div className="mb-4 flex justify-between items-center">
-        <div className="space-x-2">
-          <Button variant="ghost" size="icon">
-            <Bold className="h-4 w-4" />
-            <span className="sr-only">Bold</span>
-          </Button>
-          <Button variant="ghost" size="icon">
-            <Italic className="h-4 w-4" />
-            <span className="sr-only">Italic</span>
-          </Button>
-          <Button variant="ghost" size="icon">
-            <List className="h-4 w-4" />
-            <span className="sr-only">List</span>
-          </Button>
-          <Button variant="ghost" size="icon">
-            <AlignLeft className="h-4 w-4" />
-            <span className="sr-only">Align Left</span>
-          </Button>
+    <div className="flex items-stretch p-4 w-full">
+      <div className="w-1/2 max-w-2xl mx-auto bg-white shadow-lg rounded-lg">
+        <div className="mb-4 flex justify-between items-center py-2 px-6 bg-gray-100">
+          <div className="space-x-2">
+            <Button variant="ghost" size="icon">
+              <Bold className="h-4 w-4"/>
+              <span className="sr-only">Bold</span>
+            </Button>
+            <Button variant="ghost" size="icon">
+              <Italic className="h-4 w-4"/>
+              <span className="sr-only">Italic</span>
+            </Button>
+            <Button variant="ghost" size="icon">
+              <List className="h-4 w-4"/>
+              <span className="sr-only">List</span>
+            </Button>
+            <Button variant="ghost" size="icon">
+              <AlignLeft className="h-4 w-4"/>
+              <span className="sr-only">Align Left</span>
+            </Button>
+          </div>
+          <div className="flex items-center gap-x-2">
+            <Button variant="secondary" onClick={() => setContent(mdContentExample)}>Use example content</Button>
+            <Button onClick={handlePublish} className="text-white">
+              <Send className="h-4 w-4 mr-2"/>
+              Publish
+            </Button>
+          </div>
         </div>
-        <Button onClick={handlePublish} className="text-white">
-          <Send className="h-4 w-4 mr-2" />
-          Publish
-        </Button>
+        <Textarea
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          placeholder="Start writing your content here..."
+          className="w-full h-screen p-4 text-lg border-none focus:outline-0 resize-none"
+        />
       </div>
-      <Textarea
-        value={content}
-        onChange={(e) => setContent(e.target.value)}
-        placeholder="Start writing your content here..."
-        className="w-full h-[calc(100vh-200px)] p-4 text-lg border-none focus:outline-0 resize-none"
-      />
+
+      <div className="w-1/2 flex  flex-col items-stretch bg-white shadow-lg rounded-lg">
+        <div className="py-2 px-6 bg-gray-100">
+          <strong>Preview</strong> <span className="text-gray-500">(May not reflect how your content will be rendered)</span>
+        </div>
+        <Preview content={content} />
+      </div>
     </div>
   )
 }
